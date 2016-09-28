1d5835b74389a608bbca67c78d2814807daba4b7
Paul Kilpatrick <paulkilpatrick@MacBookPRO.gateway>
2016-09-28 16:31:25 +1000

    menu almost finalised

--- govstrap/js/scripts.js
+++ govstrap/js/scripts.js
@@ -1,1 +1,20 @@
-!function(t,a){"use strict";a.behaviors.govstrap={attach:function(t,a){}}}(jQuery,Drupal);
+!function(t,a){"use strict";a.behaviors.govstrap={attach:function(t,a){
+    //Loop over all 3rd level uls
+    jQuery('#superfish-1 > li > ul > li > ul > li').each(function() {
+        jQuery(this).addClass('level-3-menu');
+        jQuery(this).unwrap();
+    });
+
+    jQuery('#superfish-1 > li > ul > li > li').each(function() {
+        jQuery(this).unwrap();
+    });
+
+    jQuery('#superfish-1 > li > ul > a').each(function() {
+        jQuery(this).wrap('<li></li>');
+    });
+
+    jQuery('#superfish-1 > li > ul > a > span.sf-sub-indicator').each(function() {
+        jQuery(this).remove();
+    });
+
+}}}(jQuery,Drupal);
--- govstrap/src/js/scripts.js
+++ govstrap/src/js/scripts.js
@@ -10,6 +10,14 @@
     Drupal.behaviors.govstrap = {
         attach: function (context, settings) {
             // Site scripts.
+            //Loop over all 3rd level uls
+            jQuery('#superfish-1 > li > ul > li > ul').each(function() {
+                $a=$(this).children('li');
+                // Move
+                $($a.parent().parent()).after($a);
+                // Delete
+                $(this).remove();
+            });
         }
     };
 
