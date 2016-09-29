cc35a0745d3c09d0c82f27cd657b121ab260da44
Paul Kilpatrick <paulkilpatrick@MacBookPRO.gateway>
2016-09-29 11:57:12 +1000

    add in right side menu basic MVP styling

--- govstrap/templates/page--front.tpl.php
+++ govstrap/templates/page--front.tpl.php
@@ -45,24 +45,19 @@
   <div id="main" class="main-container row">
     <?php print $messages; ?>
     <div id="content">
-
-      <div class="title-block">
+      <?php if (!empty($page['sidebar_first'])): ?>
+        <aside class="col-sm-3" role="complementary">
+          <?php print render($page['sidebar_first']); ?>
+        </aside>  <!-- /#sidebar-first -->
+      <?php endif; ?>
+      <section id="main-content-section" class="<?php print $content_column_class; ?>" role="main">
+        <a id="main-content"></a>
         <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
         <?php print render($title_prefix); ?>
         <?php if (!empty($title)): ?>
           <h1 class="page-header"><?php print $title; ?></h1>
         <?php endif; ?>
         <?php print render($title_suffix); ?>
-      </div>
-
-
-      <?php if (!empty($page['sidebar_first'])): ?>
-        <aside class="col-sm-3" role="complementary">
-          <?php print render($page['sidebar_first']); ?>
-        </aside>  <!-- /#sidebar-first -->
-      <?php endif; ?>
-      <section id="main-content-section" class="<?php print $content_column_class; ?> content-section" role="main">
-        <a id="main-content"></a>
         <?php if (!empty($tabs)): ?>
           <?php print render($tabs); ?>
         <?php endif; ?>
@@ -77,7 +72,7 @@
         </div>
       </section>
       <?php if (!empty($page['sidebar_second'])): ?>
-        <aside class="col-sm-3 right-bar" role="complementary">
+        <aside class="col-sm-3" role="complementary">
           <?php print render($page['sidebar_second']); ?>
         </aside>  <!-- /#sidebar-second -->
       <?php endif; ?>
