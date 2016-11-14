0f811868cd3a04ed58b4af16bba4b3ce0d8ffe98
Joseph Zhao <joseph.zhao@xing.net.au>
2016-11-14 11:49:50 +1100

    Front page style tweaks.

--- govstrap/panels-layouts/front/panels-front.tpl.php
+++ govstrap/panels-layouts/front/panels-front.tpl.php
@@ -1,13 +1,13 @@
 
 <div class="page-template-home-1" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
   <?php if ($content['top']): ?>
-    <div class="row">
+    <div class="row page-front-top">
       <div class="inside"><?php print $content['top']; ?></div>
     </div>
   <?php endif; ?>
 
-  <div class="row">
-    <div class="col-sm-8">
+  <div class="row page-front-middle">
+    <div class="col-sm-8 page-front-middle-left">
       <div class="browse-home">
         <h3>Find information in these areas:</h3>
         <ul>
@@ -18,13 +18,13 @@
       </div>
       <div class="inside"><?php print $content['left']; ?></div>
     </div>
-    <div class="col-sm-4">
+    <div class="col-sm-4 page-front-middle-right">
       <div class="inside"><?php print $content['right']; ?></div>
     </div>
   </div>
 
   <?php if ($content['bottom']): ?>
-    <div class="row">
+    <div class="row page-front-bottom">
       <div class="inside"><?php print $content['bottom']; ?></div>
     </div>
   <?php endif; ?>
